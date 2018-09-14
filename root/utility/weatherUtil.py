9c16cdd5938adb7533ad40070e48796efb8dabb8
Tommy <tommy.cm.tang1996@gmail.com>
2018-09-14 12:09:11 +0800

    added loggng and imporve warframe module

--- root/utility/weatherUtil.py
+++ root/utility/weatherUtil.py
@@ -3,10 +3,11 @@ import datetime
 import time
 import os
 import pickle
+from .requestHTMLUtil import requestHTML
 from tkinter import ttk
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
+
 
 class GetWeatherApp(threading.Thread):
     def __init__(self, configs, container):
@@ -14,15 +15,7 @@ class GetWeatherApp(threading.Thread):
         self.setDaemon(True)
         self.container = container
         self.configs = configs
-        
-        options_Args = ["--headless", "disable-gpu"]
-        self.options = Options()
-        for arg in options_Args:
-            self.options.add_argument(arg)
-        self.service_args = ["hide_console", ]
-        self.DriverPath = __file__.rsplit(os.sep,1)[0] + r'\applications\chromedriver.exe'
         self.currentPath = __file__.rsplit(os.sep,1)[0]
-        
         self.start()
 
     def checkWarningTextCache(self):
@@ -34,7 +27,7 @@ class GetWeatherApp(threading.Thread):
         except:
             pass
         if mtime == None or ((datetime.datetime.now() - mtime).total_seconds() > 3600):
-            warningText = self.getWarning("https://www.hko.gov.hk/wxinfo/dailywx/wxwarntoday_uc.htm")
+            warningText = requestHTML("https://www.hko.gov.hk/wxinfo/dailywx/wxwarntoday_uc.htm", self.getWarningScrape)
             with open(self.currentPath+"\cache\warningText.txt", "wb") as warningTextFile:
                 pickle.dump(warningText, warningTextFile)
         else:
@@ -52,7 +45,7 @@ class GetWeatherApp(threading.Thread):
         except:
             pass
         if mtime == None or ((datetime.datetime.now() - mtime).total_seconds() > 3600):
-            weatherResultDict = self.getWeather("https://www.hko.gov.hk/contentc.htm")
+            weatherResultDict = requestHTML("https://www.hko.gov.hk/contentc.htm", self.getWeatherScrape)
             with open(self.currentPath+r"\cache\weatherResultDict.txt", "wb") as weatherResultDictFile:
                 pickle.dump(weatherResultDict, weatherResultDictFile)
         else:
@@ -67,6 +60,8 @@ class GetWeatherApp(threading.Thread):
             weatherResultDict = self.checkWeatherResultDictCache()
             with self.configs.condition:
                 if not self.configs.isStopped:
+                    for widget in self.container.winfo_children():
+                        widget.destroy()
                     r = self.showTime(0)
                     r = self.updateWarning(r, warningText)
                     r = self.updateWeather(r, weatherResultDict)
@@ -86,7 +81,6 @@ class GetWeatherApp(threading.Thread):
             r += 1
         return r
 
-    
     def updateWeather(self, startingRow, weatherResultDict):
         r = startingRow
         ttk.Label(self.container, text="日期").grid(column=0, row=r, sticky='W')
@@ -110,13 +104,6 @@ class GetWeatherApp(threading.Thread):
                 warningText.append(tag.attrs['alt'])
         return warningText
 
-    def getWarning(self, url):
-        browser = webdriver.Chrome(self.DriverPath,service_args=self.service_args, options=self.options, )
-        browser.get(url)
-        result = self.getWarningScrape(browser.page_source)
-        browser.quit()
-        return result
-
     def getWeatherScrape(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         tags = soup.findAll(True, {'class':['fnd_date', 'fnd_min', 'fnd_max']})
@@ -125,10 +112,4 @@ class GetWeatherApp(threading.Thread):
         desc = [desc.attrs['title'] for desc in descs]
         value = tuple(zip(tags[1::3], tags[2::3], desc))
         return dict(zip(tags[::3], value))
-     
-    def getWeather(self, url):
-        browser = webdriver.Chrome(self.DriverPath,service_args=self.service_args, options=self.options, )
-        browser.get(url)
-        result = self.getWeatherScrape(browser.page_source)
-        browser.quit()
-        return result
+        
